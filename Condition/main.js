3e0b8d8436a9ade7d085cffd520741c6394a318c
Célian Frasca <celian.fras@gmail.com>
2021-03-15 20:49:02 +0100

    condition permis

---
+++ Condition/main.js
@@ -0,0 +1,8 @@
+var marc = {nom:'Marc', age:18, moyenne: 15}
+if (marc.age >= 18) {
+    console.log("Tu peux passer ton permis")
+} else if (marc.age >= 15) {
+    console.log("Tu peux faire la conduite accompagnée")
+} else {
+    console.log("Tu ne peux pas encore conduire")
+}
