a20895e8ba9d67724d324293511313645adc18eb
Célian Frasca <celian.fras@gmail.com>
2021-03-15 20:58:02 +0100

    condition tarif jeune

--- Condition/condition2.js
+++ Condition/condition2.js
@@ -1,6 +1,6 @@
-var marc = {nom:'Marc', age:18, moyenne: 15}
-if (marc.age >= 18 && marc.age <= 25) {
-    console.log("Bravo ! Tu as le droit au tarif Jeune !")
+var marc = {nom:'Marc', age:72, moyenne: 15}
+if (marc.age >= 18 && marc.age <= 25 || marc.age > 70) {
+    console.log("Bravo ! Tu as le droit au tarif réduit !")
 } else {
-    console.log("Désolé le tarif Jeune ne s'applique pas sur toi...")
+    console.log("Désolé le tarif réduit ne s'applique pas sur toi...")
 }
