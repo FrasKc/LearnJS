c5c140af91ee9c955cf1d7e5a3821df9e5108345
Célian Frasca <celian.fras@gmail.com>
2021-03-15 21:13:17 +0100

    switchcase

---
+++ Condition/condition3.js
@@ -0,0 +1,14 @@
+var marc = {nom:'Marc', age:25, moyenne: 15}
+switch (marc.age) {
+    case 18:
+        console.log("Bravo tu as 18 ans !")
+        break
+    case 25:
+        console.log("Bravo tu as un quart de siècle ! ")
+        break
+    case 50:
+        console.log("Bravo tu as un demi siècle !")
+        break
+    default:
+        console.log("Bon Anniversaire !")
+}
