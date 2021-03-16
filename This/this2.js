67f1135eb488f2a9451d7fa9b84907f37ee0ef12
Célian Frasca <celian.fras@gmail.com>
2021-03-16 11:06:24 +0100

    This

--- This/this2.js
+++ This/this2.js
@@ -1,14 +1,14 @@
 var eleve = {
   nom: 'Jean',
   present: function () {
-    var eleve = this
+    var self = this
     var demo = {
         demo: function() {
-            console.log(eleve.nom)
+            console.log(self.nom)
         }
     }
     demo.demo()
-    console.log(this.nom + ' est présent')
+    console.log(self.nom + ' est présent')
   }
 }
 eleve.present()
